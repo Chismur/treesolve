178cb3120335731212f450db55adf631a07b80ce
Matvey Malik <matikstyle@gmail.com>
2017-09-22 20:34:44 +0300

    первый комит

---
+++ Main.py
@@ -0,0 +1,48 @@
+from sklearn.ensemble import RandomForestClassifier, BaggingClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.datasets import make_circles
+import numpy as np
+from matplotlib import pyplot as plt
+plt.style.use('ggplot')
+plt.rcParams['figure.figsize'] = 10, 6
+# %matplotlib inline
+
+np.random.seed(42)
+X, y = make_circles(n_samples=500, factor=0.1, noise=0.35, random_state=42)
+X_train_circles, X_test_circles, y_train_circles, y_test_circles = train_test_split(X, y, test_size=0.2)
+
+dtree = DecisionTreeClassifier(random_state=42)
+dtree.fit(X_train_circles, y_train_circles)
+
+x_range = np.linspace(X.min(), X.max(), 100)
+xx1, xx2 = np.meshgrid(x_range, x_range)
+y_hat = dtree.predict(np.c_[xx1.ravel(), xx2.ravel()])
+y_hat = y_hat.reshape(xx1.shape)
+plt.contourf(xx1, xx2, y_hat, alpha=0.2)
+plt.scatter(X[:,0], X[:,1], c=y, cmap='autumn')
+plt.title("Дерево решений")
+plt.show()
+
+b_dtree = BaggingClassifier(DecisionTreeClassifier(),n_estimators=300, random_state=42)
+b_dtree.fit(X_train_circles, y_train_circles)
+
+x_range = np.linspace(X.min(), X.max(), 100)
+xx1, xx2 = np.meshgrid(x_range, x_range)
+y_hat = b_dtree.predict(np.c_[xx1.ravel(), xx2.ravel()])
+y_hat = y_hat.reshape(xx1.shape)
+plt.contourf(xx1, xx2, y_hat, alpha=0.2)
+plt.scatter(X[:,0], X[:,1], c=y, cmap='autumn')
+plt.title("Бэггинг(дерево решений)")
+plt.show()
+
+rf = RandomForestClassifier(n_estimators=300, random_state=42)
+rf.fit(X_train_circles, y_train_circles)
+
+x_range = np.linspace(X.min(), X.max(), 100)
+xx1, xx2 = np.meshgrid(x_range, x_range)
+y_hat = rf.predict(np.c_[xx1.ravel(), xx2.ravel()])
+y_hat = y_hat.reshape(xx1.shape)
+plt.contourf(xx1, xx2, y_hat, alpha=0.2)
+plt.scatter(X[:,0], X[:,1], c=y, cmap='autumn')
+plt.title("Случайный лес")
+plt.show()
